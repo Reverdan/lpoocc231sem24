18c613a3c2ff37a2eb282a6c8f4efbfd299dad3a
Reverdan Almeida Sparinger <reverdan@gmail.com>
2024-03-26 21:34:45 -0300

    Projeto número primo
    Iniciado projeto.
    Comunicação por construtor e get
    Encapsulamento

---
+++ Primo/src/modelo/Controle.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class Controle
+{
+    private String numero;
+    private String mensagem;
+    private String resposta;
+
+    public Controle(String numero)
+    {
+        this.numero = numero;
+    }
+    
+    private void Executar()
+    {
+        
+    }
+
+    public String getMensagem()
+    {
+        return mensagem;
+    }
+
+    public String getResposta()
+    {
+        return resposta;
+    }
+    
+    
+}
---
+++ Primo/src/modelo/Validacao.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class Validacao
+{
+    private String numero;
+    private int num;
+    private String mensagem;
+
+    public Validacao(String numero)
+    {
+        this.numero = numero;
+    }
+    
+    private void Executar()
+    {
+        
+    }
+
+    public int getNum()
+    {
+        return num;
+    }
+
+    public String getMensagem()
+    {
+        return mensagem;
+    }
+    
+    
+}
