a964a5f2d4d6c51060873e467e08180586300800
Reverdan Almeida Sparinger <reverdan@gmail.com>
2024-04-18 21:49:44 -0300

    Término triângulos
    Herança de construtor

---
+++ Triangulos/src/modelo/AbsPropriedades.java
@@ -0,0 +1,32 @@
+package modelo;
+
+import java.util.Objects;
+
+public abstract class AbsPropriedades
+{
+    public String lado1;
+    public String lado2;
+    public String lado3;
+    public Double l1;
+    public Double l2;
+    public Double l3;
+    public String mensagem;
+    public String resposta;
+
+    public AbsPropriedades(String lado1, String lado2, String lado3)
+    {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    public AbsPropriedades(Double l1, Double l2, Double l3)
+    {
+        this.l1 = l1;
+        this.l2 = l2;
+        this.l3 = l3;
+    }
+    
+    
+      
+}
